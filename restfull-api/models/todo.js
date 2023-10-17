bc46b035ed036eecac86bafc2e1505d29a387935
msm-faraj <farajzademeysam@gmail.com>
2023-10-17 21:59:08 +0200

    added timestamp to schema

--- restfull-api/models/todo.js
+++ restfull-api/models/todo.js
@@ -1,26 +1,19 @@
 const mongoose = require("mongoose");
 
-const todoSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    // match: /^.{0,256}$/,
-    required: true,
-    trim: true,
+const todoSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      match: /^.{0,256}$/,
+      required: true,
+      trim: true,
+    },
+    isDone: {
+      type: Boolean,
+      default: false,
+    },
   },
-  createdAt: {
-    //how to make it immutable!!!??
-    type: Date,
-    default: Date.now,
-  },
-  todoDate: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-  isDone: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Todo", todoSchema);
