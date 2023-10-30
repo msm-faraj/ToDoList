c1ec9b3540196aea1216bfde2ef3145ef78a562f
msm-faraj <farajzademeysam@gmail.com>
2023-10-30 10:23:06 +0100

    deleted async-try-catch middleware

--- restfull-api/middleware/async-try-catch.js
+++
@@ -1,9 +0,0 @@
-module.export = function (routeHandler) {
-  return async (req, res, next) => {
-    try {
-      await routeHandler(req, res);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
