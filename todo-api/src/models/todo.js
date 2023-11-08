d66a5d0ca15eea77bfdb1e9e6d05015561815d8b
msm-faraj <farajzademeysam@gmail.com>
2023-11-08 15:24:22 +0100

    joining validation and schema logic together

--- todo-api/src/models/todo.js
+++ todo-api/src/models/todo.js
@@ -1,23 +1,40 @@
+const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const todoSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      match: /^.{0,256}$/,
-      required: true,
-      trim: true,
+const Todo = mongoose.model(
+  "Todo",
+  new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        minlength: 3,
+        maxlength: 255,
+        required: true,
+        trim: true,
+      },
+      isDone: {
+        type: Boolean,
+        default: false,
+      },
+      isImportant: {
+        type: Boolean,
+        default: false,
+      },
+      deletedAt: {
+        type: Date,
+        default: null,
+      },
     },
-    isDone: {
-      type: Boolean,
-      default: false,
-    },
-    deletedAt: {
-      type: Date,
-      default: null,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
+  )
 );
 
-module.exports = mongoose.model("Todo", todoSchema);
+function validateTodo(reqBody) {
+  const schema = {
+    name: Joi.string().min(3).max(256).required(),
+    isDone: Joi.boolean(),
+  };
+  return Joi.validate(reqBody, schema);
+}
+module.exports.Todo = Todo;
+module.exports.validate = validateTodo;
--- todo-api/src/modules/validator.js
+++
@@ -1,9 +0,0 @@
-const Joi = require("joi");
-
-module.exports = (reqBody) => {
-  const schema = {
-    name: Joi.string().min(3).max(256).required(),
-    isDone: Joi.boolean(),
-  };
-  return Joi.validate(reqBody, schema);
-};
