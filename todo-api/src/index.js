a3f843bb13bbd43f19dcaa790d4f793144fd9fc2
msm-faraj <farajzademeysam@gmail.com>
2023-11-17 13:38:22 +0100

    docker and docker-compose DONE mongodb run in a separete container and this app can be used in any machine

--- todo-api/src/index.js
+++ todo-api/src/index.js
@@ -9,10 +9,10 @@ app.use(express.json());
 app.use("/api/todos", todosRouter);
 app.use(error);
 
-// mongoose.connect("mongodb://localhost/todo");
-mongoose.connect(
-  "mongodb+srv://msm-faraj:vWVkhcclNlpLEewG@cluster0.ekykahb.mongodb.net/"
-);
+mongoose.connect("mongodb://db:27017");
+// mongoose.connect(
+//   "mongodb+srv://msm-faraj:vWVkhcclNlpLEewG@cluster0.ekykahb.mongodb.net/"
+// );
 
 mongoose.connection.on("error", (error) => console.error(error.text));
 mongoose.connection.once("open", () => console.log("Connected to Database..."));
