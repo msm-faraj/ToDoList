a9524996afb3947dd51b9fd4fe96012ce088bdf3
msm-faraj <farajzademeysam@gmail.com>
2023-11-25 16:08:28 +0100

    finished integration tests for todo controller

--- todo-api/src/controllers/todoi.test.js
+++ todo-api/src/controllers/todoi.test.js
@@ -103,17 +103,117 @@ describe("/api/todos", () => {
       expect(res.status).toBe(201);
     });
   });
-  // describe("Patch /:id", () => {
-  //   it("should return 404 error if given id is not associated with todod in database", () => {});
-  //   it("should return 400 error if given todo is not valid", () => {});
-  //   it("should change name prop of todo with given new name in the given req for update", () => {});
-  //   it("should change isDone prop of todo with given new isDone in the given req for update", () => {});
-  //   it("should return the todo that just update and saved to the database", () => {});
-  // });
-  // describe("Delete /:id", () => {
-  //   it("should return 204 error if given id is not associated with todod in database", () => {});
-  //   it("should define deleteAt prop for todo if given id is associated with a todod in database", () => {});
-  //   it("should save todo to the database with new deltedAt prop", () => {});
-  //   it("should return status code 204 after soft delete a todo", () => {});
-  // });
+  describe("Patch /:id", () => {
+    it("should return 404 error if given id is not valid id", async () => {
+      const res = await request(server)
+        .patch("/api/todos/1")
+        .send({ name: "valid-name" });
+      expect(res.status).toBe(404);
+    });
+    it("should return 404 error if given id is not associated with todod in database", async () => {
+      const res = await request(server)
+        .patch("/api/todos/6561ccc4c7f982b3fadaa535")
+        .send({ name: "valid-name" });
+      expect(res.status).toBe(404);
+    });
+    it("should return 400 error if given tods name has less tan 3 char", async () => {
+      await Todo.collection.insertMany([
+        {
+          _id: new mongoose.mongo.ObjectId("6561ccc4c7f982b3fadaa535"),
+          name: "todo1",
+          isDone: false,
+          createdAt: 1,
+        },
+      ]);
+      const res = await request(server)
+        .patch("/api/todos/6561ccc4c7f982b3fadaa535")
+        .send({ name: "12" });
+      expect(res.status).toBe(400);
+    });
+    it("should return 400 error if given tods name has more than 255 char", async () => {
+      await Todo.collection.insertMany([
+        {
+          _id: new mongoose.mongo.ObjectId("6561ccc4c7f982b3fadaa535"),
+          name: "todo1",
+          isDone: false,
+          createdAt: 1,
+        },
+      ]);
+      const res = await request(server)
+        .patch("/api/todos/6561ccc4c7f982b3fadaa535")
+        .send({
+          name: "1234573698798;sdlkfjg;sldfkgj;lsdkfjg;lsdkfjg;lakjfg;alksdfjg;lakjsd;lfajs;dlfkj;alskdjf;laksdjf;lakjsd;lfkj;alskdfj;laskdjf;laksjdlfkja;lskdjf76987698273465987236948576928374569283746598273465982736459872364985726394875692873465982734695872346958723694857692837465982376459872364598723649587263948756928734569283746",
+        });
+      expect(res.status).toBe(400);
+    });
+
+    it("should change name prop of todo with given new name in the given req for update", async () => {
+      await Todo.collection.insertMany([
+        {
+          _id: new mongoose.mongo.ObjectId("6561ccc4c7f982b3fadaa535"),
+          name: "todo1",
+          isDone: false,
+          createdAt: 1,
+        },
+      ]);
+      const res = await request(server)
+        .patch("/api/todos/6561ccc4c7f982b3fadaa535")
+        .send({
+          name: "new-name",
+        });
+      expect(res.body.name).toBe("new-name");
+    });
+    it("should change isDone prop of todo with given new isDone in the given req for update", async () => {
+      await Todo.collection.insertMany([
+        {
+          _id: new mongoose.mongo.ObjectId("6561ccc4c7f982b3fadaa535"),
+          name: "todo1",
+          isDone: false,
+          createdAt: 1,
+        },
+      ]);
+      const res = await request(server)
+        .patch("/api/todos/6561ccc4c7f982b3fadaa535")
+        .send({
+          name: "new-name",
+          isDone: true,
+        });
+      expect(res.body.isDone).toBe(true);
+    });
+  });
+  describe("Delete /:id", () => {
+    beforeEach(async () => {
+      await Todo.collection.insertMany([
+        {
+          _id: new mongoose.mongo.ObjectId("6561ccc4c7f982b3fadaa535"),
+          name: "todo1",
+          isDone: false,
+          createdAt: 1,
+          deletedAt: null,
+        },
+      ]);
+    });
+    it("should return 404 error if given id is not valid id", async () => {
+      const res = await request(server).delete("/api/todos/1");
+      expect(res.status).toBe(404);
+    });
+    it("should return 204 error if given id is not associated with todod in database", async () => {
+      const res = await request(server).delete(
+        "/api/todos/6561ccc4c7f982b3fadaa536"
+      );
+      expect(res.status).toBe(204);
+    });
+    it("should define deleteAt prop for todo if given id is associated with a todod in database", async () => {
+      const res = await request(server).delete(
+        "/api/todos/6561ccc4c7f982b3fadaa535"
+      );
+      expect(res.status).toBe(204);
+    });
+    it("should return status code 204 after soft delete a todo", async () => {
+      const res = await request(server).delete(
+        "/api/todos/6561ccc4c7f982b3fadaa535"
+      );
+      expect(res.status).toBe(204);
+    });
+  });
 });
