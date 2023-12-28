729fdc0051d79885babe070c500ebaa451c5b507
msm-faraj <farajzademeysam@gmail.com>
2023-12-28 06:30:45 +0100

    debug docker compose up

--- todo-server/src/index.js
+++ todo-server/src/index.js
@@ -23,7 +23,7 @@ mongoose.connection.once("open", () =>
   console.log(`Connected to ${db} Database...`)
 );
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 );
